3a8afebee694762f5dc790d5d0e7fe817a4372dd
6tunahan <163830281+6tunahan@users.noreply.github.com>
2025-11-10 13:29:50 +0300

    Update Next.js config for correct GitHub Pages path

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   reactCompiler: true,
-  assetPrefix: '.',
-  basePath: '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
